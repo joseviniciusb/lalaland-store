0cf69c601c419a8e3e787ee413e1af3155227498
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-04-17 13:40:58 +0000

    setup: instalar nodemon

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello World!" })
+  res.send({ message: "Teste nodemon!" })
 })
 
 app.listen(port, () => {
