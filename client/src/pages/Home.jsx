5b479b9155ed3069e753f9514e1ad8416a2d0063
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-04-17 17:15:07 +0000

    fx: testar diferentes tamanhos de fonte do tailwind

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 class="text-4xl font-bold underline">Hello world!</h1>;
 }
