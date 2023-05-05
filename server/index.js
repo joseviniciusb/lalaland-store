bd7c3e2da3de8bba4883740970c72d6224879a29
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-05-05 14:11:49 +0000

    ft: adicionar rota de login e alguns configs adicionais

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
 const products = require("./routes/products");
-
+const login = require("./routes/login");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
 const port = process.env.PORT || 3001;
-
-const path = require("path");
 
 const whitelist = ["http://localhost:3000"];
 const corsOptions = {
@@ -19,17 +18,18 @@ const corsOptions = {
   },
   credentials: true,
 };
+
 app.use(cors(corsOptions));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.json());
+app.use(products);
+app.use(login);
 
 app.get("/api", (req, res) => {
-  res.send({ message: "Teste nodemon com tailwind" });
+  res.json({ message: "Teste nodemon com tailwind" });
+  console.log("tamo aeeeeeee");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at Port: ${port}`);
 });
-
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-app.use(express.json());
-app.use(products);
