a9c3de60432b6f3a9a62430403076a0088b0ba6b
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-05-05 17:05:46 +0000

    fx: manipulando algumas migrations

--- server/migrations/20230505145550-create-user.js
+++ server/migrations/20230505145550-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
