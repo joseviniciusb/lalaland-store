9c4d0f949ff07844ff6cbe3cbf3f2b09c614f24c
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-05-25 20:38:31 +0000

    style: adicionar imagem no container de produto

--- client/src/App.js
+++ client/src/App.js
@@ -30,6 +30,10 @@ function App() {
           <SideBar />
           <article class="w-60 rounded-md p-2 shadow grid gap-y-2 bg-gray-800">
             <h2 class="text-lg font-bold text-white">Título do produto</h2>
+            <figure class="max-w-lg">
+              <img class="h-auto max-w-full rounded-lg" src="https://io.convertiez.com.br/m/sandromoscoloni/shop/products/images/3067/medium/bota-worker-masculina-sandro-moscoloni-worker-amarela_21627.jpg" alt="image description" />
+              <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Especificações do produto</figcaption>
+            </figure>
             <p class="text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis excepturi minima doloremque?</p>
             <button class="rounded p-2 bg-yellow-500 justify-self-center">Visualizar</button>
           </article>
