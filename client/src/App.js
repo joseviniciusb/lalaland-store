da6dd60e018771cef91b1ba2c3bbbc615f9cdd55
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-07-17 22:00:43 +0000

    ft: fazer map do array de produtos do card menor

--- client/src/App.js
+++ client/src/App.js
@@ -9,27 +9,32 @@ import LittleOfferCard from "./components/Offers/LittleOfferCard";
 function App() {
   const [message, setMessage] = useState("");
 
-console.log('criar array de produtos');
+  console.log('criar array de produtos');
 
-const products = [
-  {
-  marca: 'Lumiss',
-  especificacoes: 'Bota masculina Adventure',
-  preco: '120,00'
-},
-{
-  marca: 'Nike',
-  especificacoes: 'Calças de Moletom Camisa masculina Nsw Club Jogger, Masculino',
-  preco: '300,00'
-},
-{
-  marca: 'Oakley',
-  especificacoes: 'Jaqueta Windbreaker Oakley Jaqueta Windbreaker Oakley, Oakley, masculino',
-  preco: '350,00'
-}
-]
+  const products = [
+    {
+      marca: 'Lumiss',
+      especificacoes: 'Bota masculina Adventure',
+      preco: '120,00'
+    },
+    {
+      marca: 'Nike',
+      especificacoes: 'Calças de Moletom Camisa masculina Nsw Club Jogger',
+      preco: '300,00'
+    },
+    {
+      marca: 'Oakley',
+      especificacoes: 'Jaqueta Windbreaker Unissex',
+      preco: '350,00'
+    },
+    {
+      marca: 'Vicbela',
+      especificacoes: 'Calça Jogger Feminina',
+      preco: '350,00'
+    }
+  ]
 
-console.log('produtos', products);
+  console.log('produtos', products);
 
   useEffect(() => {
     fetch("/api", {
@@ -55,21 +60,33 @@ console.log('produtos', products);
           <SideBar />
 
           <div className="flex h-full w-3/5 space-x-4">
-            <BigOfferCard offerCategory="Men's T-Shirts" offerDiscount="35% OFF" smallOfferText="Special Offers"  />
+            <BigOfferCard offerCategory="Men's T-Shirts" offerDiscount="35% OFF" smallOfferText="Special Offers" />
             <LittleOfferCard />
           </div>
 
         </div>
-
-
         <div className="flex justify-center">
-          <article class="w-48 m-5 rounded-md p-2 shadow grid gap-y-2 bg-gray-800">
-            <figure class="max-w-lg">
-              <img class="h-auto max-w-full rounded-lg" src="./assets/images/bota.png" alt="Bota" />
-              <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Especificações do produto</figcaption>
-            </figure>
-          </article>
+          {
+            products.map((product) => {
+              return (
+
+                <article class="w-48 m-5 rounded-md p-2 shadow grid gap-y-2 bg-gray-800">
+                  <figure class="max-w-lg">
+                    <img class="h-auto max-w-full rounded-lg" src="./assets/images/bota.png" alt="Bota" />
+                    <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
+                      <h1>{product.marca}</h1>
+                      <h2>{product.especificacoes}</h2>
+                      <span>{` R$ ${product.preco}`}</span>
+                    </figcaption>
+                  </figure>
+                </article>
+
+              )
+
+            })
+          }
         </div>
+
 
       </Background >
 
