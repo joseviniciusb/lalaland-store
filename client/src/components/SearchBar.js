05a362bd8ea7c98edc5e6bbb3cc9b1c2e0f48223
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-07-20 22:32:03 +0000

    ft: deixar a lupa dentro do input do searchbar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -7,8 +7,8 @@ const SearchBar = () => {
                     className="block w-full px-4 py-2 text-gray-300 bg-gray-800 rounded-md focus:outline-none "
                     placeholder="Search..."
                 />
-                <button className="px-4 text-white bg-gray-700  rounded ">
-                    <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                <button className="relative right-2 w-10 text-white bg-gray-700  rounded ">
+                    <svg aria-hidden="true" className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     <span className="sr-only">Search</span>
                 </button>
             </div>
