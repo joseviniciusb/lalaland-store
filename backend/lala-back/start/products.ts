b5b8272e95ae90cbf9c35656bf60829a37a27e63
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-07-28 23:19:55 +0000

    feat: adicionar produtos no array em hard code por enquanto

--- backend/lala-back/start/products.ts
+++ backend/lala-back/start/products.ts
@@ -1,10 +1,28 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/api/products', ()=> {
-    const products = [{
-        name: 'tenis',
-        brand: 'Nike',
-        price: '25.70'
-    }]
+    const products = [
+        {
+          marca: 'Lumiss',
+          especificacoes: 'Bota masculina Adventure',
+          preco: '120,00'
+        },
+        {
+          marca: 'Nike',
+          especificacoes: 'Calças de Moletom Camisa masculina Nsw Club Jogger',
+          preco: '300,00'
+        },
+        {
+          marca: 'Oakley',
+          especificacoes: 'Jaqueta Windbreaker Unissex',
+          preco: '350,00'
+        },
+        {
+          marca: 'Vicbela',
+          especificacoes: 'Calça Jogger Feminina',
+          preco: '350,00'
+        }
+      ]
+      
     return products
 } )
